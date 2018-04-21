3e56d7db8b450bb91ba520c710b33c20b1603577
Jaden-Giordano <thejadenjack@gmail.com>
2018-04-21 17:33:58 -0400

    Managers

---
+++ scenes/GameManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+export(String) var care_scene
+export(String) var dungeon_scene
+
+func _enter_dungeon():
+    remove_child(get_node("CareFacility"))
+    add_child(load(dungeon_scene).instance())
---
+++ scenes/care/CareManager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+signal _enter_dungeon_triggered
+
+func _enter_dungeon_clicked():
+    emit_signal("_enter_dungeon_triggered")
