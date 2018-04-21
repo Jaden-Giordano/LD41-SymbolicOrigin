c2285728db62f6d68da3bf1f10b62b974c3b1004
Jaden-Giordano <thejadenjack@gmail.com>
2018-04-21 14:35:50 -0400

    replaced map manager

---
+++ Rooms/map_manager.gd
@@ -0,0 +1,13 @@
+extends TileMap
+
+signal _door_trigger
+
+func _ready():
+    connect("_door_trigger", get_parent(), "_DoorEntered")
+    pass
+
+func _door_entered(body, direction):
+    if body.get_name() == "Player":
+        emit_signal("_door_trigger", direction)
+        pass
+    pass
