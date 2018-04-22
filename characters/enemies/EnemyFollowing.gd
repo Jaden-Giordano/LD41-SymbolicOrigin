44fa19e0f2f4c25c7934fe5b545fb5fed9f74fd0
Lodugh <lajow1@gmx.de>
2018-04-22 17:15:30 +0200

    movement and collider adjustments

--- characters/enemies/EnemyFollowing.gd
+++ characters/enemies/EnemyFollowing.gd
@@ -13,15 +13,15 @@ func _physics_process(delta):
 
 func _on_MoveTimer_timeout():
 	playerPosition = get_node("/root/Game/Dungeon/Player").get_global_position()
-	if playerPosition.x - get_global_position().x > 24 :
+	if playerPosition.x - get_global_position().x > 12 :
 		movedir.x = 1
-	elif playerPosition.x - get_global_position().x < -24:
+	elif playerPosition.x - get_global_position().x < -12:
 		movedir.x = -1
 	else:
 		movedir.x = 0
-	if playerPosition.y - get_global_position().y > 24 :
+	if playerPosition.y - get_global_position().y > 12 :
 		movedir.y = 1
-	elif playerPosition.y - get_global_position().y < -24:
+	elif playerPosition.y - get_global_position().y < -12:
 		movedir.y = -1
 	else:
 		movedir.y = 0
