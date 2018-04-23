02cb1e32fabb4bf2da61d69b48d1d61b3feb109d
Jaden-Giordano <thejadenjack@gmail.com>
2018-04-23 19:36:41 -0400

    fixing random on drop factory

--- scenes/DropFactory.gd
+++ scenes/DropFactory.gd
@@ -3,7 +3,7 @@ extends Node
 export(PoolStringArray) var pickups
 
 func create_drop(position):
-    var p = randi() % (pickups.size() - 1)
+    var p = randi() % (pickups.size())
     var drop = load(pickups[p]).instance()
     drop.position = position
     get_node("../Dungeon").add_child(drop)
